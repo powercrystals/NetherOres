b75c41fbd851815610a6db2e6d7ae7ad8c914786
powercrystals <powcrystals@gmail.com>
2013-02-20 17:42:23 -0500

    Added word "Ore" to the ores that didn't have it

--- src/powercrystals/netherores/net/ClientProxy.java
+++ src/powercrystals/netherores/net/ClientProxy.java
@@ -15,11 +15,11 @@ public class ClientProxy implements INetherOresProxy
 	{
         MinecraftForgeClient.preloadTexture(NetherOresCore.terrainTexture);
 
-        LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 0), "Nether Coal");
-        LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 1), "Nether Diamond");
+        LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 0), "Nether Coal Ore");
+        LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 1), "Nether Diamond Ore");
         LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 2), "Nether Gold Ore");
         LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 3), "Nether Iron Ore");
-        LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 4), "Nether Lapis Lazuli");
+        LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 4), "Nether Lapis Lazuli Ore");
         LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 5), "Nether Redstone Ore");
         LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 6), "Nether Copper Ore");
         LanguageRegistry.addName(new ItemStack(NetherOresCore.blockNetherOres, 1, 7), "Nether Tin Ore");
