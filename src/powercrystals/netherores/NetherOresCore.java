fc3c5920e6e71486aa57a30bcc53499733d03b51
powercrystals <powcrystals@gmail.com>
2013-04-15 08:36:20 -0400

    Promoting to 2.1.1

--- src/powercrystals/netherores/NetherOresCore.java
+++ src/powercrystals/netherores/NetherOresCore.java
@@ -48,7 +48,7 @@ connectionHandler = ConnectionHandler.class)
 public class NetherOresCore extends BaseMod
 {
 	public static final String modId = "NetherOres";
-	public static final String version = "1.5.1R2.1.1B1";
+	public static final String version = "1.5.1R2.1.1";
 	public static final String modName = "Nether Ores";
 	
 	public static final String mobTexureFolder = "/textures/mob/powercrystals/netherores/";
